75838c463d20d07ec88da084e2254b41761b32a2
CyberPunkMind <17183199+zvimosh@users.noreply.github.com>
2023-01-21 21:34:14 +0200

    test

--- spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
+++ spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
@@ -4,7 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+-import org.springframework.boot.test.context.SpringBootTest;
 
+-@SpringBootTest
+ class ApplicationIntegrationTest {
+
+   @Test
+   void contextLoads() {
+     // empty test that would fail if our Spring configuration does not load correctly
+   }
+ }
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
