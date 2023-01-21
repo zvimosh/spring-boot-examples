cb66b09acf5222027c36f8c254f10a3e3b37c926
CyberPunkMind <17183199+zvimosh@users.noreply.github.com>
2023-01-21 21:36:09 +0200

    Update ApplicationTests.java

--- spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
+++ spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
